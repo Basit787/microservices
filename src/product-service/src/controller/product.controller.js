d3e952bec518aa312347f1d77ff1873d62b8d8c4
Abdul-Basit <basitsaleem282@gmail.com>
2025-04-28 12:46:35 +0530

    min changes in prettier

--- src/product-service/src/controller/product.controller.js
+++ src/product-service/src/controller/product.controller.js
@@ -4,12 +4,8 @@ export const addProduct = async (req, res) => {
   try {
     const product = req.body;
     await SendQueue("PRODUCT_QUEUE", product);
-    return res
-      .status(201)
-      .json({ message: "Product added and event sent to queue", product });
+    return res.status(201).json({ message: "Product added and event sent to queue", product });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error while adding product", error });
+    return res.status(500).json({ message: "Error while adding product", error });
   }
 };
--- src/user-service/src/controller/users.controller.js
+++ src/user-service/src/controller/users.controller.js
@@ -4,9 +4,7 @@ export const addUser = async (req, res) => {
   const user = req.body;
   try {
     await SendQueue("USER_QUEUE", user);
-    return res
-      .status(201)
-      .json({ message: "User registered and event sent to queue", user });
+    return res.status(201).json({ message: "User registered and event sent to queue", user });
   } catch (error) {
     return res.status(500).json({ message: "Failed to add user", error });
   }
